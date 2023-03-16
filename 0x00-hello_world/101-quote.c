ccbe0ce111085a55290c629805aaae44b32be4dc
FredrickNganga <fredricknganga94@gmail.com>
2023-03-16 15:50:19 -0700

    Print, followed by a newline, to the standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,7 +3,8 @@
 /**
  * main - Entry point
  *
- * Description:  prints exactly "and that piece of art is useful\" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error"
+ * Description:  prints exactly "and that piece of art is useful\" - Dora Korpar, 
+ * 2015-10-19, followed by a new line, to the standard error"
  *
  * Return: Always 1 (Error)
  */
